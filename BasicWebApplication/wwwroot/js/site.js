3f64b261dae662b1798108fe7eb09566d2a52f6b
Arezoo-Yousefi <are_yousefi@yahoo.com>
2023-05-12 17:52:19 -0700

    Add project files.

---
+++ BasicWebApplication/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+function redColor() {
+    let adeleName = Array.from(document.getElementsByClassName("adele"));
+    console.log(adeleName);
+    adeleName.forEach((element) => {
+        element.style.color = "red";
+    })
+}
+function blackColor() {
+    let adeleName = Array.from(document.getElementsByClassName("adele"));
+    adeleName.forEach(ele => ele.style.color = "black");
+}
+// Write your Javascript code.
